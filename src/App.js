b54af3904e5384273b01bfc14176be770be3ed9c
David Mejia <davidflores126@unitec.edu>
2020-09-16 02:27:51 -0600

    LogIn agregado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Agregar from './Components/Agregar';
 import Principal from './Components/Principal';
+import LogIn from './Components/LogIn';
 
 function App() {
   return (
     <div className="App">
-     <Principal/>
+     <LogIn />
     </div>
   );
 }
---
+++ src/Components/LogIn.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import logoImg from "../img1.jpeg";
+
+
+function LogIn() {
+
+
+    return (
+        <body className="h-screen bg-gray-300">
+            <div className="flex items-center justify-center h-full ">
+                <div className="flex max-w-sm md:max-w-3xl shadow-2xl border-0">
+                    <div className="hidden md:block w-1/2 relative">
+                        <img src={logoImg} alt="" className="w-full h-full  object-cover"/>
+                    </div>
+                    <div className="bg-white md:w-1/2 p-8">
+                        <h1 className="text-gray-900 text-3xl text-left font-bold">LogIn</h1>
+                        <p className="text-gray-600 text-sm  my-4 font-sans">Bienvenido, inicie sesión por favor</p>
+                        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 text-gray-600">
+                            <div className="my-3">
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Nombre de usuario" />
+                            </div>
+                            <div class="my-3">
+                                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Contraseña" />
+                            </div>
+                            <div class="flex items-center justify-between my-3">
+                                <button class="bg-purple-900 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                                    Iniciar sesión
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </body>
+
+    )
+}
+
+export default LogIn
+
+
+/*<div className="w-full max-w-xs flex content-start">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+                </div>
+                <div class="mb-6">
+                    <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+                </div>
+                <div class="flex items-center justify-between">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                        Sign In
+                    </button>
+                </div>
+            </form>
+</div>*/
+
