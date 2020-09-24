d9e0c576d51242cbd05d65e77efab7d3c0da8438
BraulioCalix55 <djbrzhn1@hotmail.com>
2020-09-24 16:58:41 -0600

    login

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -27,20 +27,15 @@ import { Link } from 'react-router-dom';
 
 
 function LogIn() {
-
-
     return (
         <body className="h-screen bg-gray-300">
             <div className="flex items-center justify-center h-full ">
                 <div className="flex max-w-sm md:max-w-3xl shadow-2xl border-0">
                     <div className="hidden md:block w-1/2 relative">
-                    
-                     
                     <img src={logoImg}  className="w-full h-full  object-cover" />
                     </div>
-                    <div className="bg-white md:w-1/2 p-8">
-                   
-                        <p id="texto" className="texto_responsive text-gray-900 text-3xl text-left font-bold"></p>
+                    <div className="bg-white md:w-1/2 p-8"> 
+                        <p id="texto" className="text-gray-900 text-3xl text-left font-bold"></p>
                         <p className="text-gray-600 text-sm  my-4 font-sans">Bienvenido, inicie sesión por favor</p>
                         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 text-gray-600">
                             <div className="my-3">
@@ -57,7 +52,6 @@ function LogIn() {
                                 </Link>
                             </div>
                         </form>
-                       
                         <div class= "sm:block w-full justify-right mx-8">
                         <picture>
                             <source 
@@ -72,20 +66,14 @@ function LogIn() {
                                     alt="">
                                 </img>
                         </picture>
-
-                            
                         </div>
                     </div>
                 </div>
             </div>
         </body>
-
     )
 }
-
 export default LogIn
-
-
 /*<div className="w-full max-w-xs flex content-start">
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div className="mb-4">
@@ -101,4 +89,3 @@ export default LogIn
                 </div>
             </form>
 </div>*/
-
