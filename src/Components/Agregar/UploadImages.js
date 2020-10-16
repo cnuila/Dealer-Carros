5b2f8151d51856da126b468ff78bf2a924d770df
Carlos Nuila <cnuila14@icloud.com>
2020-10-16 01:49:20 -0600

    Reconstruyendo Agregar

--- src/Components/Agregar/UploadImages.js
+++ src/Components/Agregar/UploadImages.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { storage } from '../firebase';
+//import { storage } from '/src/firebase';
 
 
 export default class MultipleImageUploadComponent extends Component {
